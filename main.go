19215504367d08057d4df26f72408de1550081c8
ferueda <fe.rueda@outlook.com>
2021-10-07 23:23:19 -0300

    Update background color and main logic

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import (
 	"image/color"
 	"log"
+	"sync"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 const (
 	screenWidth, screenHeight = 640, 360
-	boidCount                 = 2
+	boidCount                 = 500
+	viewRadius                = 13
+	adjRate                   = 0.015
 )
 
 var (
-	green = color.RGBA{R: 10, G: 255, B: 50, A: 255}
-	boids [boidCount]*Boid
+	birdColor = color.RGBA{R: 40, G: 40, B: 43, A: 255}
+	bgColor   = color.RGBA{R: 69, G: 123, B: 157, A: 255}
+	boids     [boidCount]*Boid
+	boidMap   [screenWidth + 1][screenHeight + 1]int
+	rwLock    = sync.RWMutex{}
 )
 
 type Game struct{}
@@ -24,11 +30,13 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(bgColor)
+
 	for _, boid := range boids {
-		screen.Set(int(boid.position.x+1), int(boid.position.y), green)
-		screen.Set(int(boid.position.x-1), int(boid.position.y), green)
-		screen.Set(int(boid.position.x), int(boid.position.y-1), green)
-		screen.Set(int(boid.position.x), int(boid.position.y+1), green)
+		screen.Set(int(boid.position.x+1), int(boid.position.y), birdColor)
+		screen.Set(int(boid.position.x-1), int(boid.position.y), birdColor)
+		screen.Set(int(boid.position.x), int(boid.position.y-1), birdColor)
+		screen.Set(int(boid.position.x), int(boid.position.y+1), birdColor)
 	}
 }
 
@@ -37,6 +45,13 @@ func (g *Game) Layout(_, _ int) (w, h int) {
 }
 
 func main() {
+	// Fills our 2d map (boidMap) with -1
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+	}
+
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
